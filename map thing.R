b20926dbba938c2f5a37381cb9d4369f09ca64d2
jaguillette <jaguillette@gmail.com>
2013-07-30 14:58:52 -0400

    Commit of local files

---
+++ map thing.R
@@ -0,0 +1,8 @@
+library(maps)
+library(geosphere)
+
+#xlim <- c(-171.738281, -56.601563)
+#ylim <- c(12.039321, 71.856229)
+#map('world', col="#DCF2DC", fill=TRUE, bg="white", lwd=0.05, xlim=xlim, ylim=ylim)
+
+map('world', col="#DCF2DC", fill=TRUE, bg="white", lwd=0.05)
