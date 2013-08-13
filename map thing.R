ca5e572afb91baed4b7d6cb57ab328678fe8b3f7
Jeremy Guillette <Jeremy@sourdough.cfa.harvard.edu>
2013-08-13 09:34:23 -0400

    changed control flow for map drawing, color scheme for lines, formatting for dot map.

--- map thing.R
+++ map thing.R
@@ -29,24 +29,28 @@ affil=affil[order(affil$totalCount),]
 
 maxCount <- max(affil$totalCount)
 
-pal <- colorRampPalette(c("#F2F2F2", "blue"))
+pal <- colorRampPalette(c("#FFD7Dc", "#9E0004"))
 colors <- pal(100)
 
 #These lines draw a map limited by the maximum and minimum values for longitude and latitude within the data provided. 
 #Comment out and use other map line for full world map.
 
-#Boundaries for continental US
-#xlim <- c(-130.07813, -60.82031)
-#ylim <- c(25.16517, 49.15297)
+LatLongView = 'United States'
 
-#boundaries for Europe
-xlim <- c(-21.621094, 43.769531)
-ylim <- c(35.460670, 75.501722)
-
-map('world', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, xlim=xlim, ylim=ylim)
-
-#Draws a full world map. Use preceding three lines for limited full map.
-#map('world', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, wrap=TRUE)
+switch(LatLongView,
+       'United States'={
+         xlim <- c(-130.07813, -60.82031)
+         ylim <- c(25.16517, 49.15297)
+         map('state', fill=TRUE, col="#DCF2DC", bg='#BBCBFA', lwd=.75)
+       },
+       'Europe'={
+         xlim <- c(-21.621094, 43.769531)
+         ylim <- c(35.460670, 75.501722)
+         map('world', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, xlim=xlim, ylim=ylim)
+       },
+       'World'={
+         map('world', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, wrap=TRUE)
+       })
 
 #Draws lines from central point described
 for (j in 1:length(affil$lat)) {
@@ -56,11 +60,11 @@ for (j in 1:length(affil$lat)) {
     inter <- gcIntermediate(c(base$lng, base$lat), c(drawLng, drawLat), n=100, breakAtDateLine=TRUE, addStartEnd=TRUE)
     colindex <- round((affil[j,]$totalCount / maxCount) * length(colors))
     if(length(inter)==2){
-      lines(inter[[1]], col=colors[colindex], lwd=1.2)
-      lines(inter[[2]], col=colors[colindex], lwd=1.2)
+      lines(inter[[1]], col=colors[colindex], lwd=1.5)
+      lines(inter[[2]], col=colors[colindex], lwd=1.5)
     }
     else {
-      lines(inter, col=colors[colindex], lwd=1.2)
+      lines(inter, col=colors[colindex], lwd=1.5)
     }
   }
 }
@@ -84,8 +88,13 @@ for (i in 1:nrow(affil)) {
 }
 
 #world_data <- map_data("world", lwd=0.05)
-#world_map <- ggplot() + geom_polygon(data=world_data, aes(x=long, y=lat), fill='#DCF2DC', colour='#BBCBFA')
-#world_map <- world_map + geom_point(data=affil, aes(x=lng, y=lat, size=size, colour=type))
-#world_map + theme_bw() + theme(panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
 
+#world_map <- ggplot() + 
+#  geom_polygon(data=world_data, aes(x=long, y=lat), fill='#DCF2DC', colour='#BBCBFA')
 
+#world_map <- world_map + 
+#  geom_point(data=affil, aes(x=lng, y=lat, size=size, colour=type))
+
+#world_map + 
+#  theme_bw() + 
+#  theme(panel.grid.major.y = element_blank(), panel.grid.major.x = element_blank())
