3e9fa51c84818f71ec3f1d84b7ed79881748f39c
Jeremy Guillette <Jeremy@sourdough.cfa.harvard.edu>
2013-08-15 10:09:21 -0400

    changed how line map works so that it uses ggplot2. Also modified some aesthetics of final map.

--- map thing.R
+++ map thing.R
@@ -27,21 +27,30 @@ colors <- pal(100)
 #These lines draw a map limited by the maximum and minimum values for longitude and latitude within the data provided. 
 #Comment out and use other map line for full world map.
 
-LatLongView = 'United States'
+LatLongView = 'World'
+
+state_data <- map_data("state")
+world_data <- map_data("world")
+world_map_01 <- ggplot() + 
+  geom_polygon(data=world_data, aes(x=long, y=lat, group=group), fill='#DCF2DC', colour='grey') +
+  geom_polygon(data=state_data, aes(x=long, y=lat, group=group), fill='#DCF2DC', colour='grey')
+#BBCBFA
 
 switch(LatLongView,
        'United States'={
          xlim <- c(-130.07813, -60.82031)
          ylim <- c(25.16517, 49.15297)
-         map('state', fill=TRUE, col="#DCF2DC", bg='#BBCBFA', lwd=.75)
+         #map('state', fill=TRUE, col="#DCF2DC", bg='#BBCBFA', lwd=.75)
        },
        'Europe'={
          xlim <- c(-21.621094, 43.769531)
          ylim <- c(35.460670, 75.501722)
-         map('world', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, xlim=xlim, ylim=ylim)
+         #map('world', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, xlim=xlim, ylim=ylim)
        },
        'World'={
-         map('world', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, wrap=TRUE)
+         xlim <- c(-180, 180)
+         ylim <- c(-90, 90)
+         #map('state', col="#DCF2DC", fill=TRUE, bg="#BBCBFA", lwd=0.05, wrap=TRUE)
        })
 
 #Draws lines from central point described
@@ -49,17 +58,27 @@ for (j in 1:length(affil$lat)) {
   drawLat <- affil[j,]$lat
   drawLng <- affil[j,]$lng
   if((xlim[1]<=drawLng & drawLng<=xlim[2])&(ylim[1]<=drawLat & drawLat<=ylim[2])){
-    inter <- gcIntermediate(c(base$lng, base$lat), c(drawLng, drawLat), n=100, breakAtDateLine=TRUE, addStartEnd=TRUE)
+    inter <- gcIntermediate(c(base$lng, base$lat), c(drawLng, drawLat), n=50, breakAtDateLine=TRUE, addStartEnd=TRUE)
     colindex <- round((affil[j,]$totalCount / maxCount) * length(colors))
     if(length(inter)==2){
-      lines(inter[[1]], col=colors[colindex], lwd=1.5)
-      lines(inter[[2]], col=colors[colindex], lwd=1.5)
+      inter_01 <- as.data.frame(inter[[1]])
+      inter_02 <- as.data.frame(inter[[2]])
+      names(inter_01) <- c("long", "lat")
+      names(inter_02) <- c("long", "lat")
+      world_map_01 <- world_map_01 + geom_line(data=inter_01, aes(x=long, y=lat), color=colors[colindex], lwd=.5)
+      world_map_01 <- world_map_01 + geom_line(data=inter_02, aes(x=long, y=lat), color=colors[colindex], lwd=.5)
     }
     else {
-      lines(inter, col=colors[colindex], lwd=1.5)
+      #lines(inter, col=colors[colindex], lwd=1.5)
+      inter <- as.data.frame(inter)
+      names(inter) <- c("long", "lat")
+      world_map_01 <- world_map_01 + geom_line(data=inter, aes(x=long, y=lat), color=colors[colindex], lwd=.5)
     }
   }
 }
+
+world_map_01
+
 dev.off()
 
 
@@ -74,7 +93,7 @@ for (i in 1:nrow(affil)) {
   affil$type[i] = sample(1:5, 1)
 }
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #world_data <- map_data("world", lwd=0.05)
 
 #world_map <- ggplot() + 
@@ -82,9 +101,13 @@ for (i in 1:nrow(affil)) {
 
 #world_map <- world_map + 
 #  geom_point(data=affil, aes(x=lng, y=lat, size=size, colour=type))
-=======
+#=======
+state_data <- map_data("state")
 world_data <- map_data("world")
-world_map <- ggplot() + geom_polygon(data=world_data, aes(x=long, y=lat, group=group), fill='#DCF2DC', colour='grey')
+world_map <- ggplot() + 
+  geom_polygon(data=world_data, aes(x=long, y=lat, group=group), fill='#DCF2DC', colour='grey') +
+  geom_polygon(data=state_data, aes(x=long, y=lat, group=group), fill='#DCF2DC', colour='grey')
+  #BBCBFA
 world_map <- world_map + geom_point(data=affil, aes(x=lng, y=lat, size=totalCount, colour=factor(type))) + scale_colour_brewer(palette="Set1")
 world_map <- world_map + theme(panel.grid.major.y = element_blank(), 
                                panel.grid.major.x = element_blank(), 
@@ -94,8 +117,8 @@ world_map
 # now for a more globe-like map!
 # A little off, especially at the N Pole, but pretty cool!
 
-world_map + coord_map("ortho", orientation=c(30, -50, 0))
->>>>>>> efb97baff3459d9ebe9e2d15ff9bf5e6e86d0caa
+world_map + coord_map("ortho", orientation=c(42, -71, 0))
+#>>>>>>> efb97baff3459d9ebe9e2d15ff9bf5e6e86d0caa
 
 #world_map + 
 #  theme_bw() + 
